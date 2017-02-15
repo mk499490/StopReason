345fad6583eac242d127de06afe9a594650a1e4b
mk499490 <mk499490@gmail.com>
2017-02-15 15:46:27 +0900

    再起動のコマンド追加

--- src/main/java/com/horizonbagel/stopreason/Main.java
+++ src/main/java/com/horizonbagel/stopreason/Main.java
@@ -5,9 +5,9 @@ import org.bukkit.plugin.java.JavaPlugin;
 /**
  * Created by mk499490 on 2017/01/08.
  */
-public class Main extends JavaPlugin{
+public class Main extends JavaPlugin {
     @Override
-    public void onEnable(){
+    public void onEnable() {
         this.getCommand("stop").setExecutor(new StopReason());
         this.getCommand("restart").setExecutor(new RestartReason());
         this.getLogger().info("Plugin has been enabled!");
